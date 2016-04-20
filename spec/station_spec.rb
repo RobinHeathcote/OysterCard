bb88aa41720b40482d6fb28399f6054db2ebd7e2
Robin Heathcote <robin.heathcote@icloud.com>
2016-04-20 10:29:25 +0100

    Challenge 13: Added station class and name and zone

--- spec/station_spec.rb
+++ spec/station_spec.rb
@@ -1,5 +1,14 @@
 require 'station'
 
 describe Station do
+  subject(:station) {described_class.new(:station, :zone) }
+
+it 'has a name' do
+expect(station.name).to eq :station
+end
+
+it 'has a zone' do
+  expect(station.zone).to eq :zone
+end
 
 end
